91aed25792ef480350c838508ff200f20b871104
HyeminBaek <55080554+HyeminBaek@users.noreply.github.com>
2020-07-24 17:47:47 +0900

    Create 13549.cpp

---
+++ 13549.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N, K;
+
+int dx[] = { 2,-1,1 };
+bool visited[100001];
+queue<pair<int, int>> q;
+
+int bfs()
+{
+	q.push(make_pair(N, 0));
+	visited[N] == true;
+
+	while (!q.empty())
+	{
+		int x = q.front().first;
+		int time = q.front().second;
+		q.pop();
+
+		for (int k = 0; k < 3; k++)
+		{
+			if (x == K) return time;
+			else
+			{
+				if (k > 0)
+				{
+					int nx = x + dx[k];
+					if (nx >= 0 && nx <= 100000 && visited[nx] == false)
+					{
+						visited[nx] = true;
+						q.push(make_pair(nx, time + 1));
+					}
+				}
+				else
+				{
+					int nx = x * dx[k];
+					if (nx <= 100000 && visited[nx] == false)
+					{
+						visited[nx] = true;
+						q.push(make_pair(nx, time));
+					}
+				}
+			}
+		}
+	}
+}
+void init()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N >> K;
+}
+int main()
+{
+	init();
+	cout << bfs();
+
+	return 0;
+}
