db22de61b75d13bab7a709a2d74eb6bfcec76024
HyeminBaek <55080554+HyeminBaek@users.noreply.github.com>
2020-08-16 19:44:43 +0900

    Create 10871.cpp

---
+++ 10871.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N, X; cin >> N >> X;
+	int* arr = new int[N];
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < N; i++)
+	{
+		if (arr[i] < X)
+			cout << arr[i] << " ";
+	}
+
+	return 0;
+}
