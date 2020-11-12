799e29fd467bea724a70dfa254beb2c3189b992e
HyeminBaek <55080554+HyeminBaek@users.noreply.github.com>
2020-11-12 17:49:31 +0900

    BOJ

---
+++ 3000/3047.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c; cin >> a >> b >> c;
+	char s[3];
+	for (int i = 0; i < 3; i++)
+		cin >> s[i];
+
+	if (a > b)
+	{
+		int temp = b;
+		b = a;
+		a = temp;
+	}
+	if (a > c)
+	{
+		int temp = c;
+		c = a;
+		a = temp;
+	}
+	if (b > c)
+	{
+		int temp = b;
+		b = c;
+		c = temp;
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (s[i] == 'A') cout << a << " ";
+		else if (s[i] == 'B') cout << b << " ";
+		else cout << c << " ";
+	}
+
+	return 0;
+}
